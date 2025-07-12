40e63d50c2c1ddc34130370864c3aa3f551696bc
Никита Теслюк <nikiteslyuk@yandex.ru>
2025-07-12 21:27:34 +0300

    добавил новые pipeline

---
+++ bot_run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+echo "[bridgeit] Git pull"
+git pull
+
+echo "[bridgeit] Activating virtual environment"
+python3 -m venv venv
+source venv/bin/activate
+
+echo "[bridgeit] Install deps"
+#pip install -r requirements.txt
+#
+#cd ..
+#git clone https://github.com/dominicprice/endplay
+#cd endplay
+#git submodule update --init --recursive
+#pip install -e .
+
+echo "[bridgeit] Exporting TG_TOKEN"
+export TG_TOKEN="7976805123:AAHpYOm43hazvkXUlDY-q4X9US18upq9uak"
+
+echo "[bridgeit] Run application"
+python3 bot.py
